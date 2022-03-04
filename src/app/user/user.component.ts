f66acab32567c2f62f538b47c70e713b66b4813c
vaibhavbhagat196 <99129066+vaibhavbhagat196@users.noreply.github.com>
2022-03-04 09:03:34 +0530

    aa

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AppUser } from '../models/AppUser';
+import { UserService } from './user.service';
 
 @Component({
   selector: 'app-user',
@@ -7,9 +9,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
 
-  constructor() { }
+  userIdInput: any;
+
+  appUser: AppUser = {
+    username: '',
+    email: '',
+    address: {},
+    phone: ''
+  };
+
+  constructor(private userService: UserService) { }
+
+  getUser = () => {
+    console.log('getUser');
+    this.userService.getUserById(this.userIdInput)
+      // .subscribe((obj) => { this.username = obj.username });
+      .subscribe(data => this.appUser = data);
+  }
 
   ngOnInit(): void {
+    // this.getUser(2);
   }
 
 }
+
+
+
+// import { Component, OnInit } from '@angular/core';
+// import { AppUser } from '../models/AppUser';
+// import { UserService } from './user.service';
+
+// @Component({
+//   selector: 'app-user',
+//   templateUrl: './user.component.html',
+//   styleUrls: ['./user.component.css']
+// })
+// export class UserComponent implements OnInit {
+
+//   appUser: AppUser = {
+//     username: '',
+//     email: '',
+//     address: {},
+//     phone: ''
+//   };
+
+//   constructor(private userService: UserService) { }
+
+//   getUsers = () => {
+//     console.log('getUsers');
+//     return this.userService.getUserById(1)
+//       .subscribe((u) => { this.appUser = u });
+//   }
+
+//   ngOnInit(): void {
+//     this.getUsers();
+//   }
+
+// }
